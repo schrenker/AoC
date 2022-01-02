6cc0158717a5a57aa5bd53815afc607b85669679
schrenker <zawadzkis95@gmail.com>
2022-01-02 20:30:28 +0100

    day 2 part 2

--- tools/math.go
+++ tools/math.go
@@ -1,19 +1,32 @@
 package tools
 
-func VarMin(nums ...int) int {
-	min := nums[0]
-	for _, v := range nums {
-		if min > v {
-			min = v
-		}
+import "sort"
+
+func GetMin(nums ...int) int {
+	sort.Ints(nums)
+	return nums[0]
+}
+
+func GetMinMultiple(amount int, nums ...int) []int {
+	sort.Ints(nums)
+	if len(nums) <= amount {
+		return nums
 	}
-	return min
+	return nums[:amount]
 }
 
 func SumAll(nums ...int) int {
 	acc := 0
 	for _, v := range nums {
 		acc += v
+	}
+	return acc
+}
+
+func MulAll(nums ...int) int {
+	acc := 1
+	for _, v := range nums {
+		acc *= v
 	}
 	return acc
 }
--- y2015/02.go
+++ y2015/02.go
@@ -19,21 +19,30 @@ func (d DayTwo) PartOne() {
 			n, _ := strconv.Atoi(num)
 			numtmp[i] = n
 		}
+		first := numtmp[0] * numtmp[1]
+		second := numtmp[0] * numtmp[2]
+		third := numtmp[1] * numtmp[2]
+
+		acc += tools.SumAll(2*first, 2*second, 2*third, (tools.GetMin(first, second, third)))
 	}
 	fmt.Println(acc)
 }
 
 func (d DayTwo) PartTwo() {
 	acc := 0
-	for i, v := range tools.ReadFileBytes("input/2015/01.txt") {
-		if acc < 0 {
-			fmt.Println(i)
-			break
+	for _, line := range tools.ReadFileStringSlice("input/2015/02.txt") {
+		tmp := strings.Split(line, "x")
+		numtmp := make([]int, 3)
+		for i, num := range tmp {
+			n, _ := strconv.Atoi(num)
+			numtmp[i] = n
 		}
-		if v == '(' {
-			acc++
-		} else if v == ')' {
-			acc--
-		}
+		first := numtmp[0]
+		second := numtmp[1]
+		third := numtmp[2]
+
+		twoShort := tools.GetMinMultiple(2, first, second, third)
+		acc += (2 * tools.SumAll(twoShort...)) + tools.MulAll(first, second, third)
 	}
+	fmt.Println(acc)
 }
