88eed73e382a242f67d68000cbaa872493363959
schrenker <zawadzkis95@gmail.com>
2022-01-08 00:22:55 +0100

    day six part one

--- challenge.go
+++ challenge.go
@@ -13,4 +13,5 @@ var challenges = map[string]challenge{
 	"2015/03": y2015.DayThree{},
 	"2015/04": y2015.DayFour{},
 	"2015/05": y2015.DayFive{},
+	"2015/06": y2015.DaySix{},
 }
---
+++ y2015/06.go
@@ -0,0 +1,99 @@
+package y2015
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/schrenker/go_aoc/tools"
+)
+
+type DaySix struct{}
+
+type boolGrid [][]bool
+
+func makeBoolGrid() boolGrid {
+	grid := make(boolGrid, 1000, 1000)
+	for i := range grid {
+		grid[i] = make([]bool, 1000, 1000)
+		for j := range grid[i] {
+			grid[i][j] = false
+		}
+	}
+	return grid
+}
+
+func (b *boolGrid) turnOff(pos []int) {
+
+	for i := pos[0]; i <= pos[2]; i++ {
+		for j := pos[1]; j <= pos[3]; j++ {
+			(*b)[i][j] = false
+		}
+	}
+}
+
+func (b *boolGrid) turnOn(pos []int) {
+
+	for i := pos[0]; i <= pos[2]; i++ {
+		for j := pos[1]; j <= pos[3]; j++ {
+			(*b)[i][j] = true
+		}
+	}
+}
+
+func (b *boolGrid) toggle(pos []int) {
+
+	for i := pos[0]; i <= pos[2]; i++ {
+		for j := pos[1]; j <= pos[3]; j++ {
+
+			(*b)[i][j] = !(*b)[i][j]
+		}
+	}
+}
+
+func (b boolGrid) countLights() int {
+	acc := 0
+	for _, v := range b {
+		for _, vv := range v {
+			if vv {
+				acc++
+			}
+		}
+	}
+	return acc
+}
+
+func (b *boolGrid) parseCommand(command string) {
+	tmp := strings.Split(command, " ")
+	if tmp[0] == "toggle" {
+		b.toggle(parsePos(tmp[1], tmp[3]))
+	} else if tmp[0] == "turn" {
+		if tmp[1] == "on" {
+			b.turnOn(parsePos(tmp[2], tmp[4]))
+		} else {
+			b.turnOff(parsePos(tmp[2], tmp[4]))
+		}
+	}
+}
+
+func parsePos(start, finish string) []int {
+	tmpSta := strings.Split(start, ",")
+	tmpFin := strings.Split(finish, ",")
+	xs, _ := strconv.Atoi(tmpSta[0])
+	ys, _ := strconv.Atoi(tmpSta[1])
+	xf, _ := strconv.Atoi(tmpFin[0])
+	yf, _ := strconv.Atoi(tmpFin[1])
+	return []int{xs, ys, xf, yf}
+}
+
+func (d DaySix) PartOne() {
+	data := tools.ReadFileStringSlice("input/2015/06.txt")
+	grid := makeBoolGrid()
+	for _, v := range data {
+		grid.parseCommand(v)
+	}
+	fmt.Println(grid.countLights())
+}
+
+func (d DaySix) PartTwo() {
+}
