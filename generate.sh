6cb4da3f1f979a108c79ecbbec949cb25e801b54
schrenker <zawadzkis95@gmail.com>
2022-06-11 21:51:48 +0200

    template generation

---
+++ generate.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+mkdir "y$1"
+cp -n template "y$1/$2.go"
+
+sed -i "" "s|YYYY|y$1|g" "y$1/$2.go"
+sed -i "" "s|DD|$2|g" "y$1/$2.go"
---
+++ y2015/13.go
@@ -0,0 +1,11 @@
+package y2015
+
+type Day13 struct{}
+
+func (d Day13) PartOne() interface{} {
+	return nil
+}
+
+func (d Day13) PartTwo() interface{} {
+	return nil
+}
