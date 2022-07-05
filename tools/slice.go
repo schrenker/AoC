45d792a87aa3e01c4faa83dfc555419c0e328c9a
Sebastian Zawadzki <sebastian@zawadzki.tech>
2022-07-05 21:52:34 +0200

    reverse function

--- tools/slice.go
+++ tools/slice.go
@@ -40,3 +40,11 @@ func StringPermutations(arr []string) [][]string {
 	helper(arr, len(arr))
 	return res
 }
+
+func Reverse[T any](s []T) []T {
+	// for i, j := 0, len(s)-1; i < len(s)/2; i, j = i+1, j-1 {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+	return s
+}
