68e187067ff2ef71aed4bb9c99d78079c124f5d7
Sebastian Zawadzki <sebastian@zawadzki.tech>
2022-07-06 18:00:40 +0200

    map, filter, reduce in tools, we functional now

--- tools/slice.go
+++ tools/slice.go
@@ -41,10 +41,45 @@ func StringPermutations(arr []string) [][]string {
 	return res
 }
 
+// Reverse takes in a slice of any type.
+// It returns the slice reversed
 func Reverse[T any](s []T) []T {
 	// for i, j := 0, len(s)-1; i < len(s)/2; i, j = i+1, j-1 {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		s[i], s[j] = s[j], s[i]
 	}
 	return s
+}
+
+// Map turns a []T1 to a []T2 using a mapping function.
+// This function has two type parameters, T1 and T2.
+// This works with slices of any type.
+func Map[T1, T2 any](s []T1, f func(T1) T2) []T2 {
+	r := make([]T2, len(s))
+	for i, v := range s {
+		r[i] = f(v)
+	}
+	return r
+}
+
+// Reduce reduces a []T1 to a single value using a reduction function.
+func Reduce[T1, T2 any](s []T1, initializer T2, f func(T2, T1) T2) T2 {
+	r := initializer
+	for _, v := range s {
+		r = f(r, v)
+	}
+	return r
+}
+
+// Filter filters values from a slice using a filter function.
+// It returns a new slice with only the elements of s
+// for which f returned true.
+func Filter[T any](s []T, f func(T) bool) []T {
+	var r []T
+	for _, v := range s {
+		if f(v) {
+			r = append(r, v)
+		}
+	}
+	return r
 }
