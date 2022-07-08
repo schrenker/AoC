ff5477950881f7eacacf1b7b64126c01f3fa8a69
Sebastian Zawadzki <sebastian@zawadzki.tech>
2022-07-08 23:09:26 +0200

    generate now adds entry to challenge.go

--- generate.sh
+++ generate.sh
@@ -5,3 +5,7 @@ cp -n template "y$1/$2.go"
 
 sed -i "s|YYYY|y$1|g" "y$1/$2.go"
 sed -i "s|DD|$2|g" "y$1/$2.go"
+
+tac challenge.go | sed -e "2i\"$1/$2\": y$1.Day$2{}," | tac | tee challenge.go > /dev/null # I have no idea why, but redirecting from tac alone cleared out the file
+
+gofmt -w challenge.go
