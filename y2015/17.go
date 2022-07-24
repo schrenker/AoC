609c82ba56734507fc92c122442e3bb2fe81fbca
Sebastian Zawadzki <sebastian@zawadzki.tech>
2022-07-24 11:59:55 +0200

    day 17 part two done

--- y2015/17.go
+++ y2015/17.go
@@ -25,5 +25,25 @@ func (d Day17) PartOne() interface{} {
 }
 
 func (d Day17) PartTwo() interface{} {
-	return 0
+	target := 150
+	data := tools.Combinations(tools.ReadFileStringSlice())
+	min := make(map[int]int)
+	for _, d := range data {
+		tmp := make([]int, len(d))
+		for i, v := range d {
+			tmp[i], _ = strconv.Atoi(v)
+		}
+		if target == tools.SumAll(tmp...) {
+			min[len(tmp)]++
+		}
+	}
+
+	length := target
+	for k := range min {
+		if k < length {
+			length = k
+		}
+	}
+
+	return min[length]
 }
