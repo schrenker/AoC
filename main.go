3a7b037928482bf205f3fd5e1c74c8fefff5d799
Sebastian Zawadzki <sebastian@zawadzki.tech>
2022-10-30 01:06:26 +0200

    changed way input is passed to challenges

--- main.go
+++ main.go
@@ -14,10 +14,12 @@ func main() {
 		panic(fmt.Sprintf("3 Arguments are required [year day part], but %v were provided", len(args)))
 	}
 
+	tools.GetInput(os.Args[1], os.Args[2])
+
 	if i, _ := strconv.Atoi(args[2]); i == 1 {
-		fmt.Println(challenges[args[0]+"/"+args[1]].PartOne(tools.GetDefaultInputPath()))
+		fmt.Println(challenges[args[0]+"/"+args[1]].PartOne(tools.ReadFileBytes(tools.GetDefaultInputPath())))
 	} else if i == 2 {
-		fmt.Println(challenges[args[0]+"/"+args[1]].PartTwo(tools.GetDefaultInputPath()))
+		fmt.Println(challenges[args[0]+"/"+args[1]].PartTwo(tools.ReadFileBytes(tools.GetDefaultInputPath())))
 	} else {
 		panic(fmt.Sprintf("unrecognized part value: %v. Expected [01, 1, 02, 2]", args[2]))
 	}
--- tools/input.go
+++ tools/input.go
@@ -1,17 +1,16 @@
 package tools
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"strings"
 )
-
-func init() {
-	GetInput(os.Args[1], os.Args[2])
-}
 
 func truncateDay(day string) string {
 	if day[0] == '0' {
@@ -63,4 +62,28 @@ func GetInput(year, day string) {
 			log.Fatalln(err)
 		}
 	}
+}
+
+func GetDefaultInputPath() string {
+	p, err := exec.Command("go", "env", "GOMOD").Output()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return strings.TrimSpace(strings.Replace(string(p), "go.mod", "", 1)) + "input/" + os.Args[1] + "/" + os.Args[2] + ".txt"
+}
+
+func ByteToStringSlice(data []byte) []string {
+	spl := strings.Split(string(data), "\n")
+	if spl[len(spl)-1] == "" {
+		return spl[:len(spl)-1]
+	}
+	return spl
+}
+
+func ReadFileBytes(path string) []byte {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return bytes.TrimSpace(data)
 }
