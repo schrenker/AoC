3b54a61deacc121f191cc7124f3521e363bff7f9
Sebastian Zawadzki <sebastian@zawadzki.tech>
2022-12-05 20:11:03 +0100

    y2022 day 5 done

---
+++ y2022/05.go
@@ -0,0 +1,124 @@
+package y2022
+
+import (
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/schrenker/AoC/tools"
+)
+
+type Day05 struct{}
+
+func getCrates(data []byte) ([][]string, int) {
+	stringData := tools.ByteToStringSlice(data)
+	crates := [][]string{}
+	index := 0
+	rgx := regexp.MustCompile(`[\[]`)
+	spaceRgx := regexp.MustCompile(`^ `)
+	for _, v := range stringData {
+		index++
+		if v == "" {
+			break
+		}
+		tmp := rgx.ReplaceAllString(strings.ReplaceAll(v, "]", " "), " ")
+		crates = append(crates, strings.Split(spaceRgx.ReplaceAllString(tmp, ""), ""))
+	}
+
+	return crates, index
+}
+
+func stackify(crates [][]string) []*tools.Stack[string] {
+	stacks := make([]*tools.Stack[string], 0)
+	for i := 0; i < len(crates)-1; i++ {
+		stacknum := 0
+		for j := 0; j < len(crates[i]); j += 4 {
+
+			if stacknum >= len(stacks) {
+				stacks = append(stacks, tools.NewStack[string]())
+			}
+
+			if crates[i][j] != " " {
+				stacks[stacknum].Push(crates[i][j])
+			}
+
+			stacknum++
+		}
+	}
+	for i := range stacks {
+		stacks[i].Reverse()
+	}
+	return stacks
+}
+
+func parseInstructions(index int, data []byte) [][]int {
+	instructions := make([][]int, 0)
+	stringData := tools.ByteToStringSlice(data)
+	rgx := regexp.MustCompile(`\D`)
+	spaceRgx := regexp.MustCompile(`[ ]{1,}`)
+
+	for i := index; i < len(stringData); i++ {
+		tmp := rgx.ReplaceAllString(stringData[i], " ")
+		tmp = spaceRgx.ReplaceAllString(tmp, " ")
+		splitTmp := strings.Split(tmp, " ")
+
+		instruction := make([]int, 0, 3)
+
+		for _, v := range splitTmp {
+			num, err := strconv.Atoi(v)
+			if err != nil {
+				continue
+			}
+			instruction = append(instruction, num)
+		}
+
+		instructions = append(instructions, instruction)
+	}
+
+	return instructions
+}
+
+func (d Day05) PartOne(data []byte) interface{} {
+	crates, index := getCrates(data)
+	stacks := stackify(crates)
+	instructions := parseInstructions(index, data)
+
+	for i := range instructions {
+		for j := 0; j < instructions[i][0]; j++ {
+			tmp := stacks[instructions[i][1]-1].Pop()
+			stacks[instructions[i][2]-1].Push(tmp)
+		}
+	}
+
+	result := ""
+	for i := range stacks {
+		result += stacks[i].Peek()
+	}
+
+	return result
+}
+
+func (d Day05) PartTwo(data []byte) interface{} {
+	crates, index := getCrates(data)
+	stacks := stackify(crates)
+	instructions := parseInstructions(index, data)
+
+	for i := range instructions {
+		orderPreserver := make([]string, 0, instructions[i][0])
+		for j := 0; j < instructions[i][0]; j++ {
+			orderPreserver = append(orderPreserver, stacks[instructions[i][1]-1].Pop())
+		}
+		orderPreserver = tools.Reverse(orderPreserver)
+		for _, v := range orderPreserver {
+			stacks[instructions[i][2]-1].Push(v)
+		}
+	}
+
+	result := ""
+	for i := range stacks {
+		result += stacks[i].Peek()
+	}
+
+	return result
+
+}
