161689282a7bc070e93c666edbe83815065cfa1b
Sebastian Zawadzki <sebastian@zawadzki.tech>
2023-11-01 23:06:25 +0100

    go upgrade

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/schrenker/AoC
 
-go 1.18
+go 1.21.3
--- shell.nix
+++ shell.nix
@@ -4,21 +4,20 @@ with pkgs;
 
 mkShell {
   buildInputs = [
-    go_1_18
+    delve
+    go
+    gocode
     golangci-lint
+    gomodifytags
+    gopls
+    gore
+    gotests
+    gotools
     nodejs
-    shellcheck
     rnix-lsp
+    shellcheck
   ];
-
-  # shellHook = ''
-  #   go install github.com/x-motemen/gore/cmd/gore@latest
-  #   go install github.com/stamblerre/gocode@latest
-  #   go install golang.org/x/tools/cmd/godoc@latest
-  #   go install golang.org/x/tools/cmd/goimports@latest
-  #   go install golang.org/x/tools/cmd/gorename@latest
-  #   go install golang.org/x/tools/cmd/guru@latest
-  #   go install github.com/cweill/gotests/gotests@latest
-  #   go install github.com/fatih/gomodifytags@latest
-  # '';
+  shellHook = ''
+    mkdir -p .go/tmp
+  '';
 }
