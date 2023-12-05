1022524d48c654cb665932d7b0d3122aa7e5066f
Sebastian Zawadzki <sebastian@zawadzki.tech>
2023-12-05 23:59:39 +0100

    dayone done

--- y2023/05.go
+++ y2023/05.go
@@ -11,6 +11,14 @@ import (
 
 type Day05 struct{}
 
+type pair struct {
+	srcStart int
+	srcEnd int
+	dstStart int
+	dstEnd int
+	rg int
+}
+
 var spacePattern *regexp.Regexp
 var mapOrder = []string{"soil", "fertilizer", "water", "light", "temperature", "humidity", "location"}
 
@@ -28,10 +36,14 @@ func parseSeeds(s string) []int {
 
 func findLocation(seed int, m map[string]map[int]int) int {
 	loc := seed
-	for _, v := range mapOrder {
-		tmp, ok := m[v][loc]
-		if ok {
-			loc = tmp
+	for i := range mapOrder {
+		for _, p := range m[mapOrder[i]] {
+			if loc >= p.srcStart && loc <= p.srcEnd {
+				tmp := loc - p.srcStart
+				fmt.Printf("v: %v, loc: %v, p.srcStart: %v, tmp: %v, newloc: %v\n", mapOrder[i], loc, p.srcStart, tmp, p.dstStart + tmp)
+				loc = p.dstStart + tmp
+				break
+			}
 		}
 	}
 	return loc
@@ -41,12 +53,10 @@ func (d Day05) PartOne(data []byte) interface{} {
 	inp := input.ByteToStringSlice(data)
 	spacePattern = regexp.MustCompile(`\s`)
 	seeds := parseSeeds(inp[0])
-	maps := make(map[string]map[int]int)
+	maps := make(map[string][]*pair)
 	for _, v := range mapOrder {
-		maps[v] = make(map[int]int)
+		maps[v] = make([]*pair, 0)
 	}
-
-
 
 	processed := 0
 	for i := 3; i < len(inp); i++ {
@@ -56,16 +66,21 @@ func (d Day05) PartOne(data []byte) interface{} {
 			continue
 		}
 		ranges := spacePattern.Split(strings.TrimSpace(inp[i]), -1)
-		destination, _ := strconv.Atoi(ranges[0])
-		source, _ := strconv.Atoi(ranges[1])
-		length, _ := strconv.Atoi(ranges[2])
-		for j := 0; j < length; j++ {
-			maps[mapOrder[processed]][source+j] = destination + j
-		}
+		rg, _ := strconv.Atoi(ranges[2])
+		srcStart, _ := strconv.Atoi(ranges[1])
+		srcEnd := srcStart + rg - 1
+		dstStart, _ := strconv.Atoi(ranges[0])
+		dstEnd := dstStart + rg - 1
+		fmt.Printf("rg: %v, srcStart: %v, srcEnd: %v, dstStart: %v, dstEnd: %v\n", rg, srcStart, srcEnd, dstStart, dstEnd)
+		maps[mapOrder[processed]] = append(maps[mapOrder[processed]], &pair{
+			srcStart: srcStart,
+			srcEnd: srcEnd,
+			dstStart: dstStart,
+			dstEnd: dstEnd,
+			rg: rg,
+		})
 
 	}
-
-	fmt.Println("maps built")
 
 	min := 999999999999999
 	for _, v := range seeds {
