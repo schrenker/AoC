df00ad8ce86d08e254eecf55cd811ed027b70fc6
Sebastian Zawadzki <sebastian@zawadzki.tech>
2024-02-03 22:40:14 +0100

    day one of 2015

--- test/y2015/01_test.go
+++ test/y2015/01_test.go
@@ -14,6 +14,14 @@ func TestDay01PartOne(t *testing.T) {
 	os.Args = []string{"cmd", "2015", "01", "01"}
 	expected := map[string]int{
 		"1": 0,
+		"2": 0,
+		"3": 3,
+		"4": 3,
+		"5": 3,
+		"6": -1,
+		"7": -1,
+		"8": -3,
+		"9": -3,
 	}
 	for i, v := range expected {
 		if r := d.PartOne(input.ReadFileBytes(fmt.Sprintf("../testdata/y2015/01/1.%v.txt", i))); r != v {
@@ -26,7 +34,8 @@ func TestDay01PartTwo(t *testing.T) {
 	d := y2015.Day01{}
 	os.Args = []string{"cmd", "2015", "01", "02"}
 	expected := map[string]int{
-		"1": 0,
+		"1": 1,
+		"2": 5,
 	}
 	for i, v := range expected {
 		if r := d.PartTwo(input.ReadFileBytes(fmt.Sprintf("../testdata/y2015/01/2.%v.txt", i))); r != v {
--- y2015/01.go
+++ y2015/01.go
@@ -3,9 +3,33 @@ package y2015
 type Day01 struct{}
 
 func (d Day01) PartOne(data []byte) interface{} {
-	return 0
+	acc := 0
+
+	for _, v := range data {
+		if v == '(' {
+			acc++
+		} else if v == ')' {
+			acc--
+		}
+	}
+
+	return acc
 }
 
 func (d Day01) PartTwo(data []byte) interface{} {
-	return 0
+	acc := 0
+
+	for i, v := range data {
+		if v == '(' {
+			acc++
+		} else if v == ')' {
+			acc--
+		}
+
+		if acc < 0 {
+			return i + 1
+		}
+	}
+
+	return acc
 }
